5440100f8affd26cbf7e32e12efc3fe23587b4b6
LJ <lji@tradeshift.com>
2014-10-18 00:20:27 +0800

    wip

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config = {
 	/**  change this 'localhost' URL to your public URL for testing 
 		a browser accessing from outside world
 	*/
-	thirdPartyEndpoint: 'http://tranquil-chamber-5614.herokuapp.com/service',
+	thirdPartyEndpoint: 'https://tranquil-chamber-5614.herokuapp.com/service',
 	
 	/** Change this line to the exact address that Tradeshift provides to you */
 	//tradeshiftEndpoint: 'https://api.tradeshift.com/tradeshift',
