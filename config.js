3a344356549c17ee569b5c14e7e130c11ab9f3e0
LJ <lji@tradeshift.com>
2014-10-23 17:05:32 +0800

    to go.tradeshift

--- config.js
+++ config.js
@@ -8,13 +8,10 @@ config = {
 	thirdPartyEndpoint: 'http://tranquil-chamber-5614.herokuapp.com/service',
 	
 	/** Change this line to the exact address that Tradeshift provides to you */
-	//tradeshiftEndpoint: 'https://api.tradeshift.com/tradeshift',
+	tradeshiftEndpoint: 'https://api.tradeshift.com/tradeshift',
 	//tradeshiftEndpoint: 'https://api-sandbox.tradeshift.com/tradeshift',
-	tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
-	/** For Tradeshift internal developers:
-	tradeshiftEndpoint: 'https://api-apps-sandbox.tradeshift.com/tradeshift',
-	tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
-	*/
+	//tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
+
 	authId: manifest.vendor_id + '.'+ manifest.app_id
 };
 
