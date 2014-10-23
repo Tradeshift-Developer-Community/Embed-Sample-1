99dc9acef3f35cfb466d0e75e6cebd292998cb2b
LJ <lji@tradeshift.com>
2014-10-23 16:15:09 +0800

    switched to localhost for test

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ config = {
 	/**  change this 'localhost' URL to your public URL for testing 
 		a browser accessing from outside world
 	*/
-	thirdPartyEndpoint: 'https://tranquil-chamber-5614.herokuapp.com/service',
+	thirdPartyEndpoint: 'http://tranquil-chamber-5614.herokuapp.com/service',
 	
 	/** Change this line to the exact address that Tradeshift provides to you */
-	tradeshiftEndpoint: 'https://api.tradeshift.com/tradeshift',
+	//tradeshiftEndpoint: 'https://api.tradeshift.com/tradeshift',
 	//tradeshiftEndpoint: 'https://api-sandbox.tradeshift.com/tradeshift',
-	//tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
+	tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
 	/** For Tradeshift internal developers:
 	tradeshiftEndpoint: 'https://api-apps-sandbox.tradeshift.com/tradeshift',
 	tradeshiftEndpoint: 'http://localhost:8889/tradeshift-proxy',
